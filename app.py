44e81e712e84bc6b78180782b9141e302e5e4777
s-yeddula <syeddula@arize.com>
2025-02-12 23:32:14 -0800

    Update app.py

--- app.py
+++ app.py
@@ -112,4 +112,4 @@ with gr.Blocks() as demo:
             )
 
 if __name__ == '__main__':
-    demo.launch(server_name="0.0.0.0", server_port=int(os.getenv("PORT", 7860))
+    demo.launch(server_name="0.0.0.0", server_port=int(os.getenv("PORT", 7860)))
