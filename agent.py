033a357d9481dce6d44ca65ad7d1cb03fd416493
s-yeddula <syeddula@arize.com>
2025-03-06 14:45:16 -0800

    Add files via upload

--- agent.py
+++ agent.py
@@ -5,15 +5,14 @@ from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import MemorySaver
-from langgraph.graph import END, START, StateGraph, MessagesState
+from langgraph.graph import END, START, MessagesState, StateGraph
 from langgraph.prebuilt import ToolNode
 from loguru import logger
 from openinference.instrumentation.openai import OpenAIInstrumentor
-from phoenix.otel import register
-
 from rag import initialize_vector_store
-# from tools import create_rag_response, analyze_rag_response, web_search
-#from tools import initialize_tool_llm
+from tools import analyze_rag_response, create_rag_response, initialize_tool_llm, web_search
+
+from phoenix.otel import register
 
 load_dotenv()
 open_ai_llm = None
@@ -36,14 +35,11 @@ def initialize_agent_llm(model):
     """
     Initialize the agent llm, this will bind the tools to the agent llm.
     """
-    from tools import create_rag_response, analyze_rag_response, web_search
-    tools = [
-        create_rag_response,
-        analyze_rag_response,
-        web_search
-    ]
+    from tools import analyze_rag_response, create_rag_response, web_search
+
+    tools = [create_rag_response, analyze_rag_response, web_search]
     global open_ai_llm
-    open_ai_llm = ChatOpenAI(model=model, temperature=0.7).bind_tools(tools, tool_choice='auto')
+    open_ai_llm = ChatOpenAI(model=model, temperature=0.7).bind_tools(tools, tool_choice="auto")
 
 
 def router(state: MessagesState) -> str:
@@ -51,12 +47,12 @@ def router(state: MessagesState) -> str:
     This function will route the agent flow from one node to another.
     Either it will route from Agent --> Tools or Agent --> User, Agent --> END
     """
-    messages = state['messages']
+    messages = state["messages"]
     last_message = messages[-1]
-    if hasattr(last_message, 'tool_calls') and last_message.tool_calls:
-        return 'tools'
-    if type(last_message) == HumanMessage:
-        return 'agent'
+    if hasattr(last_message, "tool_calls") and last_message.tool_calls:
+        return "tools"
+    if type(last_message) is HumanMessage:
+        return "agent"
     return END
 
 
@@ -71,7 +67,7 @@ def user_input(state: MessagesState) -> dict:
         dict: A dictionary containing the processed user input.
 
     """
-    messages = state['messages']
+    messages = state["messages"]
     last_message = messages[-1].content
     print(f"Agent: {last_message}")
     q = input("Human: ")
@@ -83,7 +79,7 @@ def call_llm(state):
     This function will call the agent llm and return the response
     This is the node where the agent llm is called
     """
-    messages = state['messages']
+    messages = state["messages"]
     response = open_ai_llm.invoke(messages)
     return {"messages": [response]}
 
@@ -92,11 +88,7 @@ def construct_agent():
     """
     This function will construct the agent workflow. It will include the all the nodes in agent workflow.
     """
-    tool_node = ToolNode([
-        create_rag_response,
-        analyze_rag_response,
-        web_search
-    ])
+    tool_node = ToolNode([create_rag_response, analyze_rag_response, web_search])
 
     workflow = StateGraph(MessagesState)
     workflow.add_node("agent", call_llm)
@@ -106,13 +98,12 @@ def construct_agent():
     workflow.add_edge(START, "agent")
     workflow.add_conditional_edges("agent", router)
     workflow.add_edge("tools", "agent")
-    workflow.add_conditional_edges('user_input', router)
+    workflow.add_conditional_edges("user_input", router)
     checkpointer = MemorySaver()
     return workflow.compile(checkpointer=checkpointer)
 
 
 def main():
-    from tools import initialize_tool_llm
     logger.info("RAG Agent Started....")
 
     initialize_instrumentor("agentic_rag")
@@ -133,14 +124,14 @@ def main():
         {
             "messages": [
                 SystemMessage(content=SYSTEM_MESSAGE_FOR_AGENT_WORKFLOW),
-                HumanMessage(content=query)
+                HumanMessage(content=query),
             ]
         },
-        config={"configurable": {"thread_id": int(time.time())}}
+        config={"configurable": {"thread_id": int(time.time())}},
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Main Started.....")
     main()
     print("Main Completed.....")
